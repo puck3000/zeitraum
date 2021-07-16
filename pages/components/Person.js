f07dcf408430654d17514703b76b54fe5ddd7efc
Daniel Lerch <daniel@daniellerch.ch>
2021-07-16 10:05:20 +0200

    adds nullchecker

--- pages/components/Person.js
+++ pages/components/Person.js
@@ -4,14 +4,14 @@ export default function Person({ person }) {
   return (
     <figure className='mx-auto mb-8 sm:max-w-xs'>
       <Image
-        src={person.img}
+        src={person?.img}
         width='853'
         height='1280'
-        alt={`Portrait of ${person.name}`}
+        alt={`Portrait of ${person?.name}`}
       />
       <figcaption className=''>
-        <span className='block text-xl'>{person.name}</span>
-        {person.bio}
+        <span className='block text-xl'>{person?.name}</span>
+        {person?.bio}
       </figcaption>
     </figure>
   )
